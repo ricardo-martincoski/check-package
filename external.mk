42a707d6e7b728f0e2d8a577b848e356a67d8164
Ricardo Martincoski <ricardo.martincoski@gmail.com>
2018-07-19 23:32:06 -0300

    external.mk: targets to test the script
    update-reference-log: to be called when the buildroot version is updated
        so new changes to the check-package that should not change its
        output can be tested.
    check-package: run all available tests in the script, listed below,
        assuming update-reference-log was already called if needed.
    
    check-package-reference-log: run the script using the default python
        interpreter and compare the output to the reference log. This is
        useful when new checks were added, to measure how many files were
        added/removed from the check, how many new warning are generated,
        etc.
    check-package-python-version: run the script using python versions 2 and
        3 and check both produce the same output. This is a sanity check.
    
    Signed-off-by: Ricardo Martincoski <ricardo.martincoski@gmail.com>

--- external.mk
+++ external.mk
@@ -1,1 +1,20 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_CHECK_PACKAGE_BAD_EXAMPLES_PATH)/package/*/*.mk))
+
+check-package: check-package-reference-log check-package-python-version
+
+update-reference-log:
+	$(BR2_EXTERNAL_CHECK_PACKAGE_BAD_EXAMPLES_PATH)/utils/run.sh -b $(CURDIR) -e $(BR2_EXTERNAL_CHECK_PACKAGE_BAD_EXAMPLES_PATH) -o $(BASE_DIR) -l $(BASE_DIR)/log.base.txt
+	cp -f $(BASE_DIR)/log.base.txt $(BR2_EXTERNAL_CHECK_PACKAGE_BAD_EXAMPLES_PATH)/log.base.txt
+
+check-package-reference-log:
+	$(BR2_EXTERNAL_CHECK_PACKAGE_BAD_EXAMPLES_PATH)/utils/run.sh -b $(CURDIR) -e $(BR2_EXTERNAL_CHECK_PACKAGE_BAD_EXAMPLES_PATH) -o $(BASE_DIR) -l $(BASE_DIR)/log.default.txt
+	$(foreach log,log.default.txt, \
+		diff -U3 $(BR2_EXTERNAL_CHECK_PACKAGE_BAD_EXAMPLES_PATH)/log.base.txt $(BASE_DIR)/$(log) > $(BASE_DIR)/log.diff; \
+	)
+
+check-package-python-version:
+	$(BR2_EXTERNAL_CHECK_PACKAGE_BAD_EXAMPLES_PATH)/utils/run.sh -b $(CURDIR) -e $(BR2_EXTERNAL_CHECK_PACKAGE_BAD_EXAMPLES_PATH) -o $(BASE_DIR) -p python2 -l $(BASE_DIR)/log.python2.txt
+	$(BR2_EXTERNAL_CHECK_PACKAGE_BAD_EXAMPLES_PATH)/utils/run.sh -b $(CURDIR) -e $(BR2_EXTERNAL_CHECK_PACKAGE_BAD_EXAMPLES_PATH) -o $(BASE_DIR) -p python3 -l $(BASE_DIR)/log.python3.txt
+	$(foreach log,log.python2.txt log.python3.txt, \
+		diff -U3 $(BR2_EXTERNAL_CHECK_PACKAGE_BAD_EXAMPLES_PATH)/log.base.txt $(BASE_DIR)/$(log) > $(BASE_DIR)/log.diff; \
+	)
---
+++ utils/run.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+main() {
+    local o opts
+    local buildroot_dir br2_external_dir logfile output_dir python_interpreter
+
+    o='b:e:l:o:p:'
+    opts="$(getopt -n "${my_name}" -o "${o}" -- "${@}")"
+    eval set -- "${opts}"
+
+    while [ ${#} -gt 0 ]; do
+        case "${1}" in
+        (-b)
+            buildroot_dir="$(readlink -f "${2}")"; shift 2
+            ;;
+        (-e)
+            br2_external_dir="$(readlink -f "${2}")"; shift 2
+            ;;
+        (-l)
+            logfile="${2}"; shift 2
+            ;;
+        (-o)
+            output_dir="$(readlink -f "${2}")"; shift 2
+            ;;
+        (-p)
+            python_interpreter="${2}"; shift 2
+            ;;
+        (--)
+            shift; break
+            ;;
+        esac
+    done
+    if [ ! -e "${buildroot_dir}/DEVELOPERS" ]; then
+        printf "error: %s: not a buildroot repo\n" "${buildroot_dir}" >&2; exit 1
+    fi
+    if [ ! -e "${br2_external_dir}/external.desc" ]; then
+        printf "error: %s: not a br2-external\n" "${br2_external_dir}" >&2; exit 1
+    fi
+    if [ ! -e "${output_dir}/.br-external.mk" ]; then
+        printf "error: %s: not an output dir\n" "${output_dir}" >&2; exit 1
+    fi
+    if [ ! -z "${python_interpreter}" ]; then
+        if ! which "${python_interpreter}"; then
+            printf "error: %s: not callable\n" "${python_interpreter}" >&2; exit 1
+        fi
+    fi
+    if [ ! -d $(dirname "${logfile}") ]; then
+        printf "error: %s: logfile cannot be created\n" "${logfile}" >&2; exit 1
+    fi
+
+    {
+        cd "${buildroot_dir}"
+        ${python_interpreter} ./utils/check-package --help
+        ${python_interpreter} ./utils/check-package -vvv $(git ls-files -- package) || true
+        cd "${br2_external_dir}"
+        ${python_interpreter} "${buildroot_dir}/utils/check-package" -b -vvv $(git ls-files -- package) || true
+    } 1>"${logfile}" 2>&1
+}
+
+my_name="${0##*/}"
+main "${@}"
