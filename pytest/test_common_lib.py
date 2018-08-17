2a80b6d1d670161f1a0fea808fc283797ee191fa
Ricardo Martincoski <ricardo.martincoski@gmail.com>
2018-08-16 23:47:03 -0300

    pytest/test_common_lib: EmptyLastLine
    Signed-off-by: Ricardo Martincoski <ricardo.martincoski@gmail.com>

--- pytest/test_common_lib.py
+++ pytest/test_common_lib.py
@@ -16,3 +16,18 @@ consecutive_empty_lines = [
 def test_empty_lines(filename, string, expected):
     warnings = util.check_file(m.ConsecutiveEmptyLines, filename, string)
     assert warnings == expected
+
+
+empty_last_line = [
+    ('any', '', []),
+    ('any', '\n', [['any:1: empty line at end of file']]),
+    ('any', '\n\n', [['any:2: empty line at end of file']]),
+    ('any', '\n\n\n', [['any:3: empty line at end of file']]),
+    ('any', ' \n\t\n', [['any:2: empty line at end of file']]),
+    ]
+
+
+@pytest.mark.parametrize("filename,string,expected", empty_last_line)
+def test_empty_last_line(filename, string, expected):
+    warnings = util.check_file(m.EmptyLastLine, filename, string)
+    assert warnings == expected
