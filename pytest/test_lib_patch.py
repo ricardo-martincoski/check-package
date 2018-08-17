41d65063203c0869e5067386385c98571ce2ea35
Ricardo Martincoski <ricardo.martincoski@gmail.com>
2018-08-17 00:47:58 -0300

    pytest/test_lib_patch: NumberedSubject
    Signed-off-by: Ricardo Martincoski <ricardo.martincoski@gmail.com>

--- pytest/test_lib_patch.py
+++ pytest/test_lib_patch.py
@@ -16,3 +16,18 @@ apply_order = [
 def test_apply_order(filename, string, expected):
     warnings = util.check_file(m.ApplyOrder, filename, string)
     assert warnings == expected
+
+
+numbered_subject = [
+    ('patch', '', []),
+    ('patch', 'Subject: [PATCH 24/105] text\n', []),
+    ('patch', 'Subject: [PATCH 24/105] text\ndiff --git a/configure.ac b/configure.ac\n', [
+     ["patch:1: generate your patches with 'git format-patch -N'", 'Subject: [PATCH 24/105] text\n']]),
+    ('patch', 'Subject: [PATCH] text\ndiff --git a/configure.ac b/configure.ac\n', []),
+    ]
+
+
+@pytest.mark.parametrize("filename,string,expected", numbered_subject)
+def test_numbered_subject(filename, string, expected):
+    warnings = util.check_file(m.NumberedSubject, filename, string)
+    assert warnings == expected
